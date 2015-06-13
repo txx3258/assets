03bdbb45a4251f0185c301033d08fb87a0895f92
txx3258 <soft_tangxiaoxian@163.com>
2015-06-13 09:21:56 +0800

    ajax_html_callback

--- js/share.js
+++ js/share.js
@@ -61,7 +61,7 @@ $.extend({
                     window.location.href=data.substring(0,index);
                 }else{
                     $(container).html(data);
-		     if (typeof fn=="function”){
+		     if (fn!=undefined){
 				fn();
 			}	
                 }
